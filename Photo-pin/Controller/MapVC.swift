73fca49f353ff8f947dfdeff84fd1295439afdf5
imnikita <imvpopov@gmail.com>
2021-02-14 21:01:56 +0200

    Initial Commit

---
+++ Photo-pin/Controller/MapVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo-pin
+//
+//  Created by Nikita Popov on 14.02.2021.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
