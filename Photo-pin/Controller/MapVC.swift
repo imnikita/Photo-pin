fa19b0e5f81f2ad42498302ef20662ef69eb392d
imnikita <imvpopov@gmail.com>
2021-02-15 09:40:55 +0200

    Location configurations are added.

--- Photo-pin/Controller/MapVC.swift
+++ Photo-pin/Controller/MapVC.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import Alamofire
+import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let regionRadius: Double = 1000
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServises()
     }
 
+    @IBAction func centerMapButtonPressed(_ sender: Any) {
+        if locationManager.authorizationStatus == .authorizedAlways || locationManager.authorizationStatus == .authorizedWhenInUse{
+            centerMapOnUserLocation()
+        }
+    }
+    
+    
+}
 
+
+extension MapVC: MKMapViewDelegate{
+    func centerMapOnUserLocation(){
+        guard let coordinate = locationManager.location?.coordinate else{ return }
+        let coordinateRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius * 2.0, longitudinalMeters: regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+}
+
+extension MapVC: CLLocationManagerDelegate{
+    func configureLocationServises(){
+        if locationManager.authorizationStatus == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        centerMapOnUserLocation()
+    }
+    
 }
 
